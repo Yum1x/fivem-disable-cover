fde4b30cca4ea812bfb873971149e183aab60197
yuM1x <hello@officialyu.me>
2021-01-11 20:31:20 -0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Yume#0001'
+description 'Disable PedCover inside an interior'
+version '1.0.0'
+
+client_script 'src/client.lua'
+
---
+++ src/client.lua
@@ -0,0 +1,16 @@
+CreateThread(function()
+    --LAZY LOADING
+    local GetRoomKeyFromEntity = GetRoomKeyFromEntity
+    local PlayerPedId = PlayerPedId
+    local SetPlayerCanUseCover = SetPlayerCanUseCover
+    local PlayerId = PlayerId
+
+    while true do
+        if GetRoomKeyFromEntity(PlayerPedId()) ~= 0 then
+            SetPlayerCanUseCover(PlayerId(), false) -- Disable cover when ped are inside an interior
+        else
+            SetPlayerCanUseCover(PlayerId(), true) -- Enable cover when ped are outside an interior.
+        end
+        Wait(2000) -- 2 seconds is a fine delay for preserve user/pc performance.
+    end
+end)
